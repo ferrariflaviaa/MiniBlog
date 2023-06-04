72334fc2d223231485fbb03ac6d7a03fc307a6aa
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-06-04 18:50:37 -0300

    feat: navbar component'

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from './pages/Home'
 import { About } from './pages/About'
+import { NavBar } from './components/NavBar';
+import { Footer } from './components/Footer';
 function App() {
   return (
     <div >
       <BrowserRouter>
+        <NavBar />
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <h3>Escreva sobre você tem interesse!</h3>
+      <p>Mini Blog &copy; 2023</p>
+    </footer>
+  )
+}
