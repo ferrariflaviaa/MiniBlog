546a6fcc3eab2833820c64d23b650da2f4aa1c89
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-06-17 15:20:29 -0300

    style: navbar and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from "./styles.module.css";
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Escreva sobre você tem interesse!</h3>
       <p>Mini Blog &copy; 2023</p>
     </footer>
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import styles from "./styles.module.css";
+
 export const NavBar = () => {
   return (
-    <nav>
-      <NavLink to='/'>
+    <nav className={styles.navbar}>
+      <NavLink to='/' className={styles.brand}>
         Mini <span>Blog</span>
       </NavLink>
-      <ul>
+      <ul className={styles.links_list}>
         <li>
-          <NavLink to='/'>
+          <NavLink to='/' className={({isActive})=> (isActive ? styles.active : "")}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to='/about'>
+          <NavLink to='/about' className={({isActive})=> (isActive ? styles.active : "")}>
             Sobre
           </NavLink></li>
       </ul>
