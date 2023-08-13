401ca2c940d0ce35d940381224b1c2729ef1ddfd
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-08-13 16:49:50 -0300

    style: refactoring the page style about

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,20 +1,29 @@
 import React from 'react'
-import styles from './styles.module.css'
 import { Link } from 'react-router-dom'
+import { Customabout } from './styles'
 
 export function About() {
   return (
-    <div className={styles.about}>
-      <h2>Sobre o Mini
+    <Customabout>
+      <h2>Bem-vindo ao Mini
         <span>Blog</span>
       </h2>
       <p>
-        Este projeto consiste em um blog feito com react no front-end e Firebase no back-end
+        Nosso espaço online foi criado com um objetivo em mente: proporcionar a você a plataforma perfeita para compartilhar suas ideias e informações com o mundo. Acreditamos que cada indivíduo carrega consigo um tesouro de conhecimento único, e é por isso que queremos oferecer um lugar onde você possa expressar suas perspectivas, explorar tópicos de interesse e conectar-se com uma comunidade ávida por aprendizado.
+        <br /> <br />
+        Seja você um entusiasta apaixonado, um curioso incansável ou alguém com expertise em determinados assuntos, Mini do Blog está aqui para dar vida às suas ideias. Compartilhe insights, narre histórias, ofereça conselhos práticos e inspire outros com suas contribuições. Afinal, o conhecimento ganha valor quando é compartilhado.
+        <br /><br />
+        Então, embarque conosco nessa jornada de descoberta e colaboração. Seu conhecimento tem o poder de impactar vidas e enriquecer mentes. Estamos animados para ver as ideias brilhantes que você trará para a mesa!
+        <br /><br />
+        Junte-se a nós no Mini do Blog e faça parte dessa comunidade vibrante de pensadores e criadores. Seu próximo post pode ser o começo de uma jornada emocionante de aprendizado mútuo. Vamos compartilhar e crescer juntos!
       </p>
+      <h3>Elabore a sua estreia no mundo das   
+        <span> publicações</span>
+      </h3>
       <Link to='/posts/create' className='btn'>
-        Criar post
+      Começar a Criar
       </Link>
-    </div>
+    </Customabout>
   )
 }
 
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,17 +1,25 @@
-.about {
+import { styled } from "styled-components";
+
+export const Customabout = styled.div`
   text-align: center;
   display: flex;
   flex-direction: column;
   align-items: center;
-}
 
-.about p {
+
+ p {
   color: #aaa;
   margin-bottom: 2em;
+  width: 60%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.about a {
+ a {
   display: block;
   margin-top: 15px;
   padding: 10px 15px;
 }
+
+`
